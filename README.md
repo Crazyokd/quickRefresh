edd9109a81b38eaf91f447040c96b3c33c77b42f
Rekord <1324596506@qq.com>
2025-01-13 12:36:24 +0000

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+binding `ctrl+i` for quick refresh and quick restart on https://www.edclub.com/sportal/*.
+
+The script is very easy but very useful for me, wish it also can help you.
--- quickRefresh.js
+++ quickRefresh.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         为TypingClub重新开始打字添加快捷键
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Crazyokd/quickRefresh
 // @version      1.0
 // @description  按Ctrl+I重新开始打字
 // @author       Rekord
@@ -16,21 +16,18 @@
     'use strict';
 
     const setShortCut = ()=>{
-        let spanElement = document.querySelector('span.edicon-refresh');
-        if (spanElement) {
-            // console.log('add shortcut');
-            document.addEventListener('keydown', function(event) {
-                if (event.ctrlKey && event.key === "i") {
-                    spanElement = document.querySelector('span.edicon-refresh');
+        // console.log('add shortcut');
+        document.addEventListener('keydown', function(event) {
+            if (event.ctrlKey && event.key === "i") {
+                // console.log('press ctrl+i');
+                event.preventDefault();
 
-                    // console.log('press ctrl+i');
-                    event.preventDefault();
-                    spanElement.click();
-                }
-            });
-        } else {
-            setTimeout(setShortCut, 300);
-        }
+                let refresh = document.querySelector('span.edicon-refresh');
+                if (refresh) refresh.click();
+                let retry = document.querySelector('.btn.navbar-goback');
+                if (retry) retry.click();
+            }
+        });
     };
     window.onload = function() {
         // try to set shortcut
